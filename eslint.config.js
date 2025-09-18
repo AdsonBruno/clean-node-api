32c37f23b663afdd765ba248f2a82cf72257a1c0
Adson Bruno <adsonbruno2@gmail.com>
2025-09-18 11:47:44 -0300

    chore: add eslint settings to allow magic numbers and return any

--- eslint.config.js
+++ eslint.config.js
@@ -9,6 +9,11 @@ export default [
             ...love.rules,
             "@typescript-eslint/explicit-function-return-type": "error",
             "@typescript-eslint/no-unnecessary-boolean-literal-compare": "error",
+            "@typescript-eslint/class-methods-use-this": "warn",
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-magic-numbers": "off",
+            "@typescript-eslint/no-unsafe-assignment": "warn",
+            "@typescript-eslint/no-unsafe-member-access": "warn"
         }   
     },
     {
