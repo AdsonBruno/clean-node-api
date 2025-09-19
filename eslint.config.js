887b41c3171343aecb80fdf126909f404084cc76
Adson Bruno <adsonbruno2@gmail.com>
2025-09-19 10:42:26 -0300

    feat: ensure SignUpController returns an error if no email is provided

--- eslint.config.js
+++ eslint.config.js
@@ -13,7 +13,8 @@ export default [
             "@typescript-eslint/no-explicit-any": "warn",
             "@typescript-eslint/no-magic-numbers": "off",
             "@typescript-eslint/no-unsafe-assignment": "warn",
-            "@typescript-eslint/no-unsafe-member-access": "warn"
+            "@typescript-eslint/no-unsafe-member-access": "warn",
+            "@typescript-eslint/strict-boolean-expressions": "off"
         }   
     },
     {
